42637f11d3e20ebffbd808e3dd1de676e9f2c339
Joost Haverkort <joost@joostit.com>
2018-01-15 09:27:45 +0100

    Initial commit. Created a new Android project in Android Studio

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# VFRadar
+(c) 2018 Joost Haverkort
+dev@joostit.com
 
-VFRadar is a Flarm, OGN and ADS-B telemetry based system to visualize local air traffic.
+VFRadar is a Flarm, OGN and ADS-B telemetry based system to visualize local air traffic. This application requires a network connection the a VFRadar-Core system to function.
---
+++ app/src/main/java/com/joostit/vfradar/OperationalActivity.java
@@ -0,0 +1,13 @@
+package com.joostit.vfradar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OperationalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operational);
+    }
+}
