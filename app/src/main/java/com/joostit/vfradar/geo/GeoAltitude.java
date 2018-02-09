24519db3b3ac03023e8d9610bb8b7e138ca901cd
Joost Haverkort <joost@joostit.com>
2018-02-09 12:23:47 +0100

    Added data folder information to the preferences page Changed the way HasAltitude is determined for a GeoFence object

--- app/src/main/java/com/joostit/vfradar/geo/GeoAltitude.java
+++ app/src/main/java/com/joostit/vfradar/geo/GeoAltitude.java
@@ -1,5 +1,7 @@
 package com.joostit.vfradar.geo;
 
+import com.joostit.vfradar.utilities.Numbers;
+
 /**
  * Created by Joost on 5-2-2018.
  */
@@ -8,37 +10,27 @@ public class GeoAltitude {
 
     private static final double ft2m = 0.3048;
 
-    private double bottomAltitudeM;
-    private double topAltitudeM;
-    private boolean hasAltitudeInfo = false;
-
-    public boolean hasAltitudeInfo(){
-        return hasAltitudeInfo;
-    }
-
+    private double bottomAltitudeM = 0;
+    private double topAltitudeM = 0;
 
     public void setBottomM(Double bottom){
         if(bottom != null) {
             bottomAltitudeM = bottom;
-            hasAltitudeInfo = true;
         }
     }
 
     public void setTopM(Double top){
         if(top != null) {
             topAltitudeM = top;
-            hasAltitudeInfo = true;
         }
     }
 
     public void setBottomFt(double bottomFeet){
         bottomAltitudeM = feetToMeter(bottomFeet);
-        hasAltitudeInfo = true;
     }
 
     public void setTopFt(double topFeet){
         topAltitudeM =  feetToMeter(topFeet);
-        hasAltitudeInfo = true;
     }
 
     public double getBottomFt(){
@@ -55,5 +47,9 @@ public class GeoAltitude {
 
     public static double feetToMeter(double feet){
         return feet * ft2m;
+    }
+
+    public boolean hasAltitudeInfo(){
+        return !(Numbers.isDoubleZero(bottomAltitudeM) && Numbers.isDoubleZero(topAltitudeM));
     }
 }
